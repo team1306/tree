c66a52b03f53574d95cabc6946ae6acb54458f06
James Tautges <jamesdtautges@gmail.com>
2015-03-16 00:19:29 -0500

    Added the Big Finish

--- tree.ino
+++ tree.ino
@@ -7,8 +7,13 @@ const int increment = 5;
 
 const int branchOnePin = 2;
 const int branchOneButtonPin = 3;
-const bool branchOneActivated = false;
+bool branchOneActivated = false;
 const int branchOneNum = 60;
+
+const int bigFinishPin = 10;
+bool bigFinish = false;
+
+Adafruit_NeoPixel branchOne = Adafruit_NeoPixel(branchOneNum, branchOnePin, NEO_GRB + NEO_KHZ800);
 
 int fadePulse(int index, int time, int maxtime, int startcolor, int endcolor, int threshold) {
   if(time < threshold*index) {
@@ -29,30 +34,74 @@ int fadePulse(int index, int time, int maxtime, int startcolor, int endcolor, in
   }
 }
 
-void updatePulse(Adafruit_NeoPixel *strip, int *times, int length, int timeLit, int threshold) {
+int updatePulse(Adafruit_NeoPixel *strip, int time, int length, int timeLit, int threshold) {
   if(time < threshold*length + timeLit) {
     for(int i=0; i<length; i++) {
       // start color should be either #000000 or #603311
-      strip->setPixelColor(i, fadePulse(i, times[i], timeLit, strip->Color(0, 0, 0), strip->Color(0, 255, 0), threshold));
-      strip->show();
+      strip->setPixelColor(i, fadePulse(i, time, timeLit, strip->Color(0, 0, 0), strip->Color(0, 255, 0), threshold));
     }
+    strip->show();
+    return 1;
   }
+  return 0;
 }
 
-Adafruit_NeoPixel branchOne = Adafruit_NeoPixel(branchOneNum, branchOnePin, NEO_GRB + NEO_KHZ800);
+int fadeFill(int index, int time, int maxtime, int startcolor, int endcolor, int threshold) {
+  if(time < threshold*index) {
+    time = 0;
+  }
+  else if(time < threshold*index + maxtime) {
+    time = time - threshold*index;
+  }
+  else {
+    time = maxtime;
+  }
+  
+  return map(time, 0, maxtime, startcolor, endcolor);
+}
+
+int updateFill(Adafruit_NeoPixel *strip, int time, int length, int timeLit, int threshold) {
+  if(time < threshold*length + timeLit) {
+    for(int i=0; i<length; i++) {
+      // start color should be either #000000 or #603311
+      strip->setPixelColor(i, fadeFill(i, time, timeLit, strip->Color(0, 0, 0), strip->Color(0, 255, 0), threshold));
+    }
+    strip->show();
+    return 1;
+  }
+  return 0;
+}
 
 void setup() {
   branchOne.begin();
   branchOne.show();
 }
 
+int i = 0;
+
 void loop() {
+  
+  // check buttons
   if(digitalRead(branchOneButtonPin)) {
     branchOneActivated = true;
   }
-  if(branchOneActivated) {
-    updatePulse(&branchOne, branchOneTimes, branchOneNum, timeLit, threshold);
+  if(digitalRead(bigFinishPin)) {
+    bigFinish = true;
+  }
+  
+  // run pulses
+  if(branchOneActivated && !bigFinish) {
+    if(!updatePulse(&branchOne, i, branchOneNum, timeLit, threshold)) {
+      branchOneActivated = false;
+    }
+  }
+  
+  // run the big finish
+  if(bigFinish) {
+    // updateFill all of the branches
+    updateFill(&branchOne, i, branchOneNum, timeLit, threshold);
   }
   
   delay(increment);
+  i++;
 }
