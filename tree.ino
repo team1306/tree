4115bfb8bd097d97d62d2bef7c3a4096f97d7a84
James Tautges <jamesdtautges@gmail.com>
2015-03-16 23:24:10 -0500

    Various changes from Monday meeting that make the pulse and fade work for the pins we ended up using. The effects are also now infinitely repeatable with every press of the buttons.

--- tree.ino
+++ tree.ino
@@ -1,17 +1,19 @@
 #include <Adafruit_NeoPixel.h>
 #include <math.h>
 
-const int timeLit = 500;
-const int threshold = 3;
-const int increment = 5;
+const int timeLit = 50;
+const int threshold = 2;
+const int increment = 1;
 
-const int branchOnePin = 2;
+const int branchOnePin = 11;
 const int branchOneButtonPin = 3;
 bool branchOneActivated = false;
-const int branchOneNum = 60;
+const int branchOneNum = 120;
+int branchOneI = 0;
 
-const int bigFinishPin = 10;
+const int bigFinishPin = 2;
 bool bigFinish = false;
+int bigFinishI = 0;
 
 Adafruit_NeoPixel branchOne = Adafruit_NeoPixel(branchOneNum, branchOnePin, NEO_GRB + NEO_KHZ800);
 
@@ -38,7 +40,7 @@ int updatePulse(Adafruit_NeoPixel *strip, int time, int length, int timeLit, int
   if(time < threshold*length + timeLit) {
     for(int i=0; i<length; i++) {
       // start color should be either #000000 or #603311
-      strip->setPixelColor(i, fadePulse(i, time, timeLit, strip->Color(0, 0, 0), strip->Color(0, 255, 0), threshold));
+      strip->setPixelColor(i, strip->Color(0, fadePulse(i, time, timeLit, 0, 255, threshold), 0));
     }
     strip->show();
     return 1;
@@ -64,7 +66,7 @@ int updateFill(Adafruit_NeoPixel *strip, int time, int length, int timeLit, int 
   if(time < threshold*length + timeLit) {
     for(int i=0; i<length; i++) {
       // start color should be either #000000 or #603311
-      strip->setPixelColor(i, fadeFill(i, time, timeLit, strip->Color(0, 0, 0), strip->Color(0, 255, 0), threshold));
+      strip->setPixelColor(i, strip->Color(0, fadeFill(i, time, timeLit, 0, 255, threshold), 0));
     }
     strip->show();
     return 1;
@@ -72,26 +74,38 @@ int updateFill(Adafruit_NeoPixel *strip, int time, int length, int timeLit, int 
   return 0;
 }
 
+void blackOut(Adafruit_NeoPixel *strip, int length) {
+  for(int i=0; i<length; i++) {
+    strip->setPixelColor(i, strip->Color(0, 0, 0));
+  }
+  strip->show();
+}
+
 void setup() {
+  pinMode(branchOneButtonPin, INPUT);
+  pinMode(bigFinishPin, INPUT);
+  pinMode(13, OUTPUT);
+  
   branchOne.begin();
   branchOne.show();
 }
 
-int i = 0;
-
 void loop() {
   
   // check buttons
-  if(digitalRead(branchOneButtonPin)) {
+  if(digitalRead(branchOneButtonPin) && !branchOneActivated) {
     branchOneActivated = true;
+    branchOneI = 0;
   }
-  if(digitalRead(bigFinishPin)) {
+  if(digitalRead(bigFinishPin) && !bigFinish) {
     bigFinish = true;
+    bigFinishI = 0;
   }
   
   // run pulses
-  if(branchOneActivated && !bigFinish) {
-    if(!updatePulse(&branchOne, i, branchOneNum, timeLit, threshold)) {
+  if(branchOneActivated /*&& !bigFinish*/) {
+    if(!updatePulse(&branchOne, branchOneI, branchOneNum, timeLit, threshold)) {
+      blackOut(&branchOne, branchOneNum);
       branchOneActivated = false;
     }
   }
@@ -99,9 +113,15 @@ void loop() {
   // run the big finish
   if(bigFinish) {
     // updateFill all of the branches
-    updateFill(&branchOne, i, branchOneNum, timeLit, threshold);
+    if(!updateFill(&branchOne, bigFinishI, branchOneNum, timeLit, threshold)) {
+      blackOut(&branchOne, branchOneNum);
+      bigFinish = false;
+    }
   }
   
-  delay(increment);
-  i++;
+  if(!bigFinish) {
+    delay(increment);
+  }
+  branchOneI++;
+  bigFinishI++;
 }
